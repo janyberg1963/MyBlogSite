a933f1a2137a2a531ed0e24cf6c12daea7d0cdbb
jeff <jeff.a.nyberg@hotmail.com>
2020-11-26 12:20:31 -0600

    Add feature to create new blog post

--- Mysite/views.py
+++ Mysite/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib.auth.models import User
 from django.utils import timezone
+from .forms import PostForm
 from .models import Post
 
 # Create your views here.
@@ -14,3 +16,19 @@ def post_detail(request, pk):
     post = Post.objects.get(pk=pk)
     stuff_for_frontend = {'post': post}
     return render(request,  'Mysite/post-detail.html', stuff_for_frontend)
+
+def post_new(request):
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            user = User.objects.get(username='jeffa')
+            post = form.save(commit=False)
+            post.author = user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+
+    else:
+        form = PostForm()
+        stuff_for_frontend = {'form': form}
+    return render(request, 'Mysite/post_edit.html',stuff_for_frontend)
